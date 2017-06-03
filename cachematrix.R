94ed8e82d73d138ddbf1172aa3aaacb77fae343d
emrsen <nesendil@gmail.com>
2017-06-03 12:27:34 +0300

    Update cachematrix.R
    Tested with b <- matrix(c(4,7,1,5,8,2,9,6,3), nrow = 3, ncol = 3)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R programming assignment for lexical scoping
+## It took time while working and studying R together resulting late in assignment
+## Thanks for our time who reviews
 
-## Write a short comment describing this function
+## This Function collect from x variable create an environment for sub funtions and store data
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(mean) m <<- mean
+        getinv <- function() m
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Check if the same matrix stored and inverse has been computed
+## If yes retrive the value without hassle else compute and store for future use
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
 }
